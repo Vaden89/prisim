f813a41ab1dcf1e25aab3cc3da45d2ab3bc9b08b
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-02 21:09:02 +0100

    updated the menu links to point to the correct page sections

--- apps/web/src/data/menu.ts
+++ apps/web/src/data/menu.ts
@@ -1,6 +1,6 @@
 export const menu = [
-  { label: "Features", href: "/features" },
-  { label: "How it works", href: "/how-it-works" },
-  { label: "For Engineering", href: "/for-engineering" },
-  { label: "Pricing", href: "/pricing" },
+  { label: "Features", href: "#features" },
+  { label: "How it works", href: "#how-it-works" },
+  { label: "For Engineering", href: "#" },
+  { label: "Pricing", href: "#" },
 ];
