67909d6b39b67aaf22f1b30088ed76f477eeaade
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-09 17:56:11 +0100

    added new colors to the design token

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,5 @@
-export * from "./components/button";
 export * from "./components/form";
+export * from "./components/button";
+export * from "./components/select";
+export * from "./components/modals";
 export { cn } from "./components/cn";
