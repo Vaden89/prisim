6dbb533ab84d9873a0d30a2889f490cc0dede4b6
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-10 16:43:50 +0100

    improved styling on the login page, added a way to route to the register page

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image";
-import { LoginForm } from "../../../components/auth/forms/login-form";
+import { LoginForm } from "@/components/auth/forms/login-form";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="w-full p-4 h-dvh grid grid-cols-1">
-      <div className="w-full flex flex-col items-center justify-center space-y-8">
-        <Link href="/">
-          <Image
-            src="/images/logo.svg"
-            width={105}
-            height={40}
-            alt="Prisim Logo"
-          />
-        </Link>
-        <span className="text-3xl text-center lg:text-4xl font-semibold">
-          Welcome back
-        </span>
+    <div className="w-full h-dvh flex flex-col items-center justify-center p-5">
+      <div className="w-full sm:w-1/2 xl:w-1/3 px-4 sm:px-8 py-6 bg-tertiary border border-border-gray rounded-xl text-center">
+        <div className="flex flex-col gap-4 mb-4">
+          <Link href="/">
+            <Image
+              width={40}
+              height={40}
+              loading="eager"
+              alt="Prisim Logo"
+              src="/images/logo-2.svg"
+              className="mx-auto"
+            />
+          </Link>
+          <span className="text-2xl text-center font-semibold">
+            Welcome back
+          </span>
+        </div>
         <LoginForm />
+        <p className="text-[13px] mt-2 text-center text-light-gray">
+          Don&apos;t have an account?{" "}
+          <Link href="/register" className="font-medium underline">
+            Register here
+          </Link>
+        </p>
       </div>
-      {/*<div className=""></div>*/}
     </div>
   );
 }
