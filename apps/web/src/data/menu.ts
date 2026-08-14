c4eb24d0f315a9b28c30dec415d770ac7f126083
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-14 17:02:16 +0100

    created menus array detailing all the routes for the dashboard, and settings for the settings section

--- apps/web/src/data/menu.ts
+++ apps/web/src/data/menu.ts
@@ -1,6 +1,31 @@
+import { SettingsItem } from "@/types/settings.type";
+import { CircleGauge, LucideIcon } from "lucide-react";
+
 export const menu = [
   { label: "Features", href: "#features" },
   { label: "How it works", href: "#how-it-works" },
   { label: "For Engineering", href: "#" },
   { label: "Pricing", href: "#" },
+];
+
+export type DashboardMenuItem = {
+  label: string;
+  icon: LucideIcon;
+  segment: string | null;
+  href: (id: string) => string;
+};
+
+export const dashboardMenu: DashboardMenuItem[] = [
+  {
+    segment: null,
+    label: "Overview",
+    icon: CircleGauge,
+    href: (id) => `/dashboard/${id}`,
+  },
+];
+
+export const workspaceSettings: SettingsItem[] = [
+  { label: "General", section: "workspace" },
+  { label: "Team Members", section: "team-members" },
+  { label: "Integrations", section: "integrations" },
 ];
