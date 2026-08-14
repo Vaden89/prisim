e15ae5bee86126ba977f1c82ebb2f328d37c0674
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-14 15:54:27 +0100

    created function to return a board and all tasks under that board, extended the getBoards function to also return the number of tasks under that board

--- packages/convex/convex/board_functions.ts
+++ packages/convex/convex/board_functions.ts
@@ -44,7 +44,90 @@ export const getBoards = query({
       )
       .collect();
 
-    return boards;
+    const boardsWithTaskCount = await Promise.all(
+      boards.map(async (board) => {
+        const tasks = await ctx.db
+          .query("tasks")
+          .withIndex("boardId", (q) => q.eq("boardId", board._id))
+          .collect();
+
+        return { ...board, taskCount: tasks.length };
+      }),
+    );
+
+    return boardsWithTaskCount;
+  },
+});
+
+export const getBoardWithTasks = query({
+  args: {
+    boardId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const authUser = await authComponent.safeGetAuthUser(ctx);
+
+    if (!authUser) {
+      return null;
+    }
+
+    const user = await ctx.db
+      .query("users")
+      .withIndex("authId", (q) => q.eq("authId", authUser._id))
+      .unique();
+
+    if (!user) {
+      return null;
+    }
+
+    const board = await ctx.db.get(args.boardId as Id<"boards">);
+
+    if (!board) {
+      return null;
+    }
+
+    const staff = await ctx.db
+      .query("staff")
+      .withIndex("org_user", (q) =>
+        q.eq("orgId", board.orgId).eq("userId", user._id),
+      )
+      .unique();
+
+    if (!staff) {
+      throw new ConvexError({
+        code: "STAFF_NOT_FOUND",
+        message: "You are not a member of this organization",
+      });
+    }
+
+    const tasks = await ctx.db
+      .query("tasks")
+      .withIndex("boardId", (q) => q.eq("boardId", board._id))
+      .collect();
+
+    const populatedTasks = await Promise.all(
+      tasks.map(async (task) => {
+        const [assignee, status] = await Promise.all([
+          ctx.db.get(task.assignee),
+          ctx.db.get(task.status),
+        ]);
+
+        return {
+          ...task,
+          assignee: assignee
+            ? {
+                _id: assignee._id,
+                firstName: assignee.firstName,
+                lastName: assignee.lastName,
+              }
+            : null,
+          status: status
+            ? { _id: status._id, name: status.name, color: status.color }
+            : null,
+        };
+      }),
+    );
+
+    return { ...board, tasks: populatedTasks };
   },
 });
 
