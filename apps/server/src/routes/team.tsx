33d34a319a9022686f1709386e068f935ec93b58
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-21 11:39:37 +0100

    added new endpoint, called to add workspace invitation emails to the email queue

---
+++ apps/server/src/routes/team.tsx
@@ -0,0 +1,38 @@
+import { Hono } from "hono";
+import { getEnv } from "../env.js";
+import { render } from "hono-email";
+import { AuthEnv } from "../types/auth.types.js";
+import { requireAuth } from "../middleware/auth.js";
+import { emailQueue } from "../queue/email.queue.js";
+import InviteToWorkspaceEmail from "../emails/invite-to-workspace.js";
+
+const teamRouter = new Hono<AuthEnv>();
+teamRouter.use(requireAuth);
+
+teamRouter.post("/invite", async (c) => {
+  const env = getEnv(c);
+  const { FRONTEND_URL } = env;
+  const { orgName, invitations } = await c.req.json();
+
+  await Promise.all(
+    invitations.map(async (invitee: { id: string; email: string }) => {
+      const { html, text } = await render(
+        <InviteToWorkspaceEmail
+          workspaceName={orgName}
+          inviteUrl={`${FRONTEND_URL}/invite/${invitee.id}`}
+        />,
+      );
+
+      await emailQueue.add("email", {
+        to: invitee.email,
+        html,
+        text,
+        subject: `Invitation to ${orgName} workspace on Prism`,
+      });
+    }),
+  );
+
+  return c.json({ message: "Invites sent successfully" }, 200);
+});
+
+export default teamRouter;
