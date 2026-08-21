50f0e6f554c5ade5943c8267f081465bdd5704f8
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-08-21 14:22:24 +0100

    added padding to the bottom of all the section so the page scrolls properly and there's enough space between the content and the end of screen, added a new section for viewing the connected codebases

--- apps/web/src/components/settings/codebase-integration-section.tsx
+++ apps/web/src/components/settings/codebase-integration-section.tsx
@@ -1,4 +1,4 @@
-export function UserProfileSection() {
+export function CodebaseIntegrationSection() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       WORK IN PROGRESS
